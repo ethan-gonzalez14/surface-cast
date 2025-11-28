43b793ed6632afbf01bcac429d486ee4fb32a361
ethan-gonzalez14 <ethangonzalez.1409@gmail.com>
2025-11-28 03:33:22 -0500

    Created "Constants.h" to keep max values and constants.

---
+++ src/Constants.h
@@ -0,0 +1,22 @@
+#ifndef Constants.h
+#define Constants.h
+
+//#include "Arduino.h"
+#include <cmath>
+#include <string>
+#include <vector>
+//All predetermined values are in CGS untis
+
+const double PI = 3.141592653;
+
+const double MAX_LASER_HEIGHT = 100.0;
+const double MAX_TRACK_LENGTH = 25.0;
+const double MAX_LASER_WIDTH_OFFSET = 20.0;
+//Maximum rotation that can be acheived by the laser
+//Ideally, but no more than, pi radians
+const double MAX_HORIZONTAL_LASER_ANGLE = PI;
+//Ideally less than pi/4 radians
+//Only the angle starting at the laser's horizontal position is measured
+const double MAX_VERTICAL_LASER_ANGLE = PI / 2;
+
+#endif
